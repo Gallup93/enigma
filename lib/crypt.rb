a4ed8ef321c9a28df048e90a90cbd4a6ef1243ff
Michael Gallup <Gallup93@icloud.com>
2020-04-18 15:57:34 -0600

    added current_date_as_int method with corresponding test

--- lib/crypt.rb
+++ lib/crypt.rb
@@ -1,2 +1,13 @@
 class Crypt
+  attr_reader :base_characters
+  def initialize
+    @base_characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i",
+                        "j", "k", "l", "m", "n", "o", "p", "q", "r",
+                        "s", "t", "u", "v", "w", "x", "y", "z", " "]
+  end
+
+  def current_date_as_int
+    time = Time.new
+    time.strftime("%d/%m/%y").gsub(/[^0-9,.]/, "").to_i
+  end
 end
--- test/crypt_test.rb
+++ test/crypt_test.rb
@@ -8,6 +8,16 @@ class CryptTest < Minitest::Test
   end
 
   def test_initialization_with_attributes
+    base_characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i",
+                        "j", "k", "l", "m", "n", "o", "p", "q", "r",
+                        "s", "t", "u", "v", "w", "x", "y", "z", " "]
+
     assert_instance_of Crypt, @crypt
+    assert_equal base_characters, @crypt.base_characters
+  end
+
+  def test_current_date_as_int
+    date = @crypt.current_date_as_int
+    assert_equal true, date.to_s.length == 5 || date.to_s.length == 6
   end
 end
