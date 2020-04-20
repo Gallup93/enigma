73623097e90add7781a333265f6b9b756a31eb35
Michael Gallup <Gallup93@icloud.com>
2020-04-20 15:49:24 -0600

    beefed up test_encrypt test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -13,6 +13,8 @@ class EnigmaTest < Minitest::Test
   def test_encrypt
     expected = {encryption: "keder ohulw", key: "02715", date: "040895"}
     assert_equal expected, @enigma.encrypt("Hello World", "02715", "040895")
+    expected = {encryption: "keder ohulw!", key: "02715", date: "040895"}
+    assert_equal expected, @enigma.encrypt("Hello World!", "02715", "040895")
   end
 
   def test_decrypt
