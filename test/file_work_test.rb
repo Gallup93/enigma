0f7e49be61dd5226be24e941327773842739258e
Michael Gallup <Gallup93@icloud.com>
2020-04-19 18:51:23 -0600

    removed obsolete code

--- test/file_work_test.rb
+++ test/file_work_test.rb
@@ -6,7 +6,6 @@ class FileWorkTest < Minitest::Test
   end
 
   def test_get_text
-    text = "this is only a test. remain calm! If you panic, you will die. This is for your own safety! This is a test."
     expected = "this is only a test remain calm if you panic you will die this is for your own safety this is a test"
     assert_equal expected, @file_work.get_text('./files/read_from_test.txt')
   end
