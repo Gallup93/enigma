1904f8303c6a2bf3e4a4c3325586734bc8db8362
Michael Gallup <Gallup93@icloud.com>
2020-04-20 22:52:46 -0600

    refactored build_and_execute

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -17,8 +17,8 @@ class Enigma
   def build_and_execute_crypt(names_key_date, type)
     nkd = names_key_date
     file_work = FileWork.new
+    text = file_work.get_text(file_work.create_file_path(nkd[0]))
     if type == :encrypt
-      text = file_work.get_text(file_work.create_file_path(nkd[0]))
       crypt = encrypt(text, nkd[2], nkd[3])
       text = crypt[:encryption]
       file_work.write_text((file_work.create_file_path(nkd[1])), text)
@@ -26,7 +26,6 @@ class Enigma
       puts "Created '#{nkd[1]}' with the key #{crypt[:key]} and date #{crypt[:date]}"
       crypt
     else
-      text = file_work.get_text(file_work.create_file_path(nkd[0]))
       crypt = decrypt(text, nkd[2], nkd[3])
       text = crypt[:decryption]
       file_work.write_text((file_work.create_file_path(nkd[1])), text)
